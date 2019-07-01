fa60a1f8a6e771e7800f26c7c3216351392d0c4f
jules <congtuyen598@gmail.com>
2019-07-02 00:27:26 +0700

    add todo filter

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem';
 import arrow from './images/down-arrow.svg';
+import classNames from 'classnames';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      newItem: '',
+      numItems: 2,
+      currentFilter: 'todoItems',
       todoItems: [
         { title: 'Đi đá bóng', isComplete: true },
         { title: 'Đi chợ' },
@@ -18,11 +22,35 @@ class App extends Component {
   onItemClicked(item) {
     return (e) => {
       const isComplete = item.isComplete;
-      const { todoItems } = this.state;
+      let { todoItems, numItems } = this.state;
       const index = todoItems.indexOf(item);
+
+      if (isComplete) {
+        numItems++;
+      }
+      else{
+        numItems--;
+      }
+
+      if (this.state.currentFilter === 'activeItems') {
+        let { activeItems } = this.state;
+        activeItems.splice(activeItems.indexOf(item), 1);
+        this.setState({
+          activeItems: activeItems
+        })
+      }
+
+      if (this.state.currentFilter === 'completedItems') {
+        let { completedItems } = this.state;
+        completedItems.splice(completedItems.indexOf(item), 1);
+        this.setState({
+          completedItems: completedItems
+        })
+      }
 
       this.setState({
         newTodo: '',
+        numItems,
         // immutability
         todoItems: [
           ...todoItems.slice(0, index),
@@ -48,12 +76,13 @@ class App extends Component {
         isComplete: false
       }
 
-      let { todoItems } = this.state;
+      let { todoItems, numItems } = this.state;
 
       todoItems.push(newItem);
 
       this.setState({
         newTodo: '',
+        numItems: numItems + 1,
         todoItems
       })
     }
@@ -65,6 +94,28 @@ class App extends Component {
     });
   }
 
+  onAllFilter(e) {
+    let { todoItems } = this.state;
+    this.setState({
+      currentFilter: 'todoItems',
+      todoItems: todoItems
+    })
+  }
+  onActiveFilter(e) {
+    let { todoItems } = this.state;
+    this.setState({
+      currentFilter: 'activeItems',
+      activeItems: todoItems.filter(item => item.isComplete !== true)
+    })
+  }
+  onCompletedFilter(e) {
+    let { todoItems } = this.state;
+    this.setState({
+      currentFilter: 'completedItems',
+      completedItems: todoItems.filter(item => item.isComplete === true)
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -74,16 +125,22 @@ class App extends Component {
         </div>
         
         {
-          this.state.todoItems.length > 0 && 
-          this.state.todoItems.map((item, index) => 
+          this.state[this.state.currentFilter].length > 0 && 
+          this.state[this.state.currentFilter].map((item, index) => 
             <TodoItem key={index} item={item} onClick={this.onItemClicked(item)} />)
         }
         {
-          this.state.todoItems.length === 0 && 'Nothing here'
+          this.state[this.state.currentFilter].length === 0 && 'Nothing here'
         }
 
         <div className="Footer">
-          <span className="TodoCount"></span>
+          <span className="TodoCount">{this.state.numItems} items left</span>
+          <ul className="TodoFilters" >
+            <li className={classNames({'active': this.state.currentFilter === 'todoItems'})} onClick={this.onAllFilter.bind(this)}>All</li>
+            <li className={classNames({'active': this.state.currentFilter === 'activeItems'})} onClick={this.onActiveFilter.bind(this)}>Active</li>
+            <li className={classNames({'active': this.state.currentFilter === 'completedItems'})} onClick={this.onCompletedFilter.bind(this)}>Completed</li>
+          </ul>
+          <button>Clear Completed</button>
         </div>
       </div>
     );
